8bebdc596162dae3ff9cb8d0f187512682114aac
Mayank Shree <mayank.shree24@gmail.com>
2026-06-17 13:27:35 +0530

    [mayank]add : recursion assignments added

---
+++ core-java-practice/gcr-codebase/recursion/PrintNTo1.java
@@ -0,0 +1,28 @@
+package recursion;
+
+public class PalindromeRecursion {
+
+    public static boolean isPalindrome(String text, int start, int end) {
+
+        if (start >= end) {
+            return true;
+        }
+
+        if (text.charAt(start) != text.charAt(end)) {
+            return false;
+        }
+
+        return isPalindrome(text, start + 1, end - 1);
+    }
+
+    public static void main(String[] args) {
+
+        String text = "madam";
+
+        if (isPalindrome(text, 0, text.length() - 1)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+    }
+}
